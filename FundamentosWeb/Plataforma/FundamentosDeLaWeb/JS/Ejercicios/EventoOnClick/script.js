a87c8570ab9bbfbcbc92a3f712bac1b86376b603
Jalcantaracanto <javier.alcantara.canto@gmail.com>
2023-03-27 12:08:23 -0300

    Avances bootcamp

---
+++ FundamentosWeb/Plataforma/FundamentosDeLaWeb/JS/Ejercicios/EventoOnClick/script.js
@@ -0,0 +1,13 @@
+// Desaparecer Boton
+function hide(element) {
+    element.remove();
+}
+
+// Cambiar Login
+function ChangeLogin(element) {
+    element.innerText = "Logout";
+}
+// Alerta
+function message() {
+    alert('Ninja was liked');
+}
