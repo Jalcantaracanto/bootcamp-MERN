b731b6250601884e110b955cf6fec01a2ab072ba
Jalcantaracanto <javier.alcantara.canto@gmail.com>
2023-03-27 23:11:30 -0300

    a

--- FundamentosWeb/Clases/Clase_5/script.js
+++ FundamentosWeb/Clases/Clase_5/script.js
@@ -1,5 +1,37 @@
 //tipos de datos
-
+//variable global
 var saludo = 'Buenas noches ninjas'
+//const y let
+//const => es un valor constante
+//let => es un valor que va a ir cambiando
 
-console.log(saludo)
+let student = 'Gabriela';
+student = 'Valentina';
+
+console.log('student: ', student);
+
+// Tipos de datos
+// STRINGS => son cadenas de texto
+const username = 'javierAlcántara'
+
+// Numbers => son números;
+// Operadores => + / - =
+const minutes = 1 * 60;
+const ageStudent1 = 40;
+const ageStudent2 = 25;
+const totalAge = ageStudent1 += ageStudent2;
+
+// Array o listas
+const students = ['Diego', 'José', 'Javier', 'Carol']
+console.log("🚀 ~ file: script.js:22 ~ students:", students)
+
+//Objetos
+// está compuesto por llave valor
+
+const stack = {
+    stackName: 'MERN',
+    instructor: 'Carolina Da Silva'
+}
+
+// Funciones
+// Es un fragmento de código responsabel de ejecutr una solucion
