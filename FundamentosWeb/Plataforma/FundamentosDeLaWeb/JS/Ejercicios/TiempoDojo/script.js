9fe70902678ebc172775a4ca76f8f0e03e64035d
Jalcantaracanto <javier.alcantara.canto@gmail.com>
2023-03-29 20:44:41 -0300

    otras soluciones

--- FundamentosWeb/Plataforma/FundamentosDeLaWeb/JS/Ejercicios/TiempoDojo/script.js
+++ FundamentosWeb/Plataforma/FundamentosDeLaWeb/JS/Ejercicios/TiempoDojo/script.js
@@ -33,6 +33,7 @@ function changeTemp(selector) {
     for (var i = 1; i <= 8; i++) {
         var temp = document.querySelector(".number-" + i);
         var tempint = parseInt(temp.innerText);
+        console.log(temp)
         console.log(tempint);
         if (selector.value == "°C") {
             temp.innerText = Math.round(((tempint - 32) * 5) / 9);
@@ -41,3 +42,44 @@ function changeTemp(selector) {
         }
     }
 }
+
+// var degrees = [
+//     document.querySelector('.number-1'),
+//     document.querySelector('.number-2'),
+//     document.querySelector('.number-3'),
+//     document.querySelector('.number-4'),
+//     document.querySelector('.number-5'),
+//     document.querySelector('.number-6'),
+//     document.querySelector('.number-7'),
+//     document.querySelector('.number-8'),
+// ]
+
+// function changeTemp(change) {
+//     var celciusDegreesMax = ['24', '27', '21', '26']
+//     var celciusDegreesMin = ['18', '19', '16', '21']
+//     var farenheitDegreesMax = ['75', '80', '69', '78']
+//     var farenheitDegreesMin = ['65', '66', '61', '70']
+
+//     console.log(celciusDegreesMax);
+//     console.log(celciusDegreesMin);
+
+//     if(change.value == "°F"){
+//         degrees[0].innerText = farenheitDegreesMax[0]
+//         degrees[1].innerText = farenheitDegreesMin[0]
+//         degrees[2].innerText = farenheitDegreesMax[1]
+//         degrees[3].innerText = farenheitDegreesMin[1]
+//         degrees[4].innerText = farenheitDegreesMax[2]
+//         degrees[5].innerText = farenheitDegreesMin[2]
+//         degrees[6].innerText = farenheitDegreesMax[3]
+//         degrees[7].innerText = farenheitDegreesMin[3]
+//     }else {
+//         degrees[0].innerText = celciusDegreesMax[0]
+//         degrees[1].innerText = celciusDegreesMin[0]
+//         degrees[2].innerText = celciusDegreesMax[1]
+//         degrees[3].innerText = celciusDegreesMin[1]
+//         degrees[4].innerText = celciusDegreesMax[2]
+//         degrees[5].innerText = celciusDegreesMin[2]
+//         degrees[6].innerText = celciusDegreesMax[3]
+//         degrees[7].innerText = celciusDegreesMin[3]
+//     }
+// }
