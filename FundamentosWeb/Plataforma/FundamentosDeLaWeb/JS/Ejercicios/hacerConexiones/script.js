778fb089afb6301b7bd76f5bc2024ae2cc1efca3
Jalcantaracanto <javier.alcantara.canto@gmail.com>
2023-03-28 23:30:21 -0300

    Mejora en Hacer Conexiones

--- FundamentosWeb/Plataforma/FundamentosDeLaWeb/JS/Ejercicios/hacerConexiones/script.js
+++ FundamentosWeb/Plataforma/FundamentosDeLaWeb/JS/Ejercicios/hacerConexiones/script.js
@@ -1,26 +1,19 @@
-var test = document.querySelector(".remove1");
-var test2 = document.querySelector(".remove2");
 var numberC = document.querySelector(".connR");
 var numberY = document.querySelector(".connY");
 var nombre = document.querySelector(".card-body h1");
 
-count = 2;
-conn = 418;
 
-function remove1() {
-    test.remove();
-    count--;
-    conn++;
-    numberC.innerHTML = count;
-    numberY.innerHTML = conn;
+function acceptC(remove) {
+    var accept = document.querySelector(remove)
+    accept.remove();
+    numberC.innerHTML--
+    numberY.innerHTML++
 }
 
-function remove2() {
-    test2.remove();
-    count--;
-    conn++;
-    numberC.innerHTML = count;
-    numberY.innerHTML = conn;
+function declineC(remove) {
+    var decline = document.querySelector(remove)
+    decline.remove();
+    numberC.innerHTML--
 }
 
 
