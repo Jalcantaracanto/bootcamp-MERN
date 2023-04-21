69e23e49c85077631746bf6dcff673abfdd4faa0
Jalcantaracanto <javier.alcantara.canto@gmail.com>
2023-04-21 11:47:55 -0400

    ejercicio apoyalo

--- MERN/Plataforma/React/ejercicios/apoyalo/src/Components/PersonCard.js
+++ MERN/Plataforma/React/ejercicios/apoyalo/src/Components/PersonCard.js
@@ -1,25 +1,33 @@
 import React, { Component } from "react"
 
 class PersonCard extends Component {
-    render(){
-        // const card = ({firstName, lastName, age, hairColor}) => {
-        //     return(
-        //         <div>
-        //             <h1>{firstName}, {lastName}</h1>
-        //             <p>Age: {age}</p>
-        //             <p>Hair Color: {hairColor}</p>
-        //         </div>
-        //     )
-        // }
-        const {firstName} = this.props
-        const {lastName} = this.props
-        const {age} = this.props
-        const {hairColor} = this.props
-        return(
+    constructor(props) {
+        super(props)
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    increaseAge = () => {
+        this.setState({
+            age: this.state.age + 1
+        })
+    }
+
+    render() {
+        const { firstName, lastName, hairColor } = this.props
+        const { age } = this.state
+
+        return (
             <div>
-                <h1>{lastName}, {firstName}</h1>
+                <h1>
+                    {lastName}, {firstName}
+                </h1>
                 <p>Age: {age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.increaseAge}>
+                    BirthDay Button for {firstName} {lastName}
+                </button>
             </div>
         )
     }
